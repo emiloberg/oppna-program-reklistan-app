f61eddd432c75cb959c79339693ad05d77fa3aca
Gustav Carlson <gustav.carlson@monator.com>
2015-05-08 17:38:11 +0200

    Missing method...

--- app/shared/utils/htmlRenderer.js
+++ app/shared/utils/htmlRenderer.js
@@ -7,7 +7,18 @@ function HtmlRenderer() {}
 	var templates = {};
 
 	function registerTemplate(name, templateContent) {
+		if (name === undefined || name === '') {
+			throw Error('Name cannot be empty!');
+		}
 		templates[name] = Handlebars.compile(templateContent);
+	}
+
+	function processTemplate(templateName, templateContent) {
+		var template = templates[templateName];
+		if (template === undefined) {
+			throw Error('No such Template registered');
+		}
+		return template(templateContent);
 	}
 
 	return {
