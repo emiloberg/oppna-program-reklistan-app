5121f36b95fd11ebad964bf216d43ad39c9e0ee3
Gustav Carlson <gustav.carlson@monator.com>
2015-05-08 21:16:32 +0200

    test sketch

--- app/shared/utils/dataSource.js
+++ app/shared/utils/dataSource.js
@@ -9,15 +9,18 @@ var utils = require('./utils');
 
 var RESOURCE_URLS = {
 		drugs: {
-			url: 'http://local.dev:8080/api/jsonws/skinny-web.skinny/get-skinny-journal-articles/company-id/10155/group-name/Guest/ddm-structure-id/11571/locale/sv_SE',
+			//url: 'http://local.dev:8080/api/jsonws/skinny-web.skinny/get-skinny-journal-articles/company-id/10155/group-name/Guest/ddm-structure-id/11571/locale/sv_SE',
+			url: 'http://localhost:5656/drugs.json',
 			isJson: true
 		},
 		advice: {
-			url: 'http://local.dev:8080/api/jsonws/skinny-web.skinny/get-skinny-journal-articles/company-id/10155/group-name/Guest/ddm-structure-id/12602/locale/sv_SE',
+			//url: 'http://local.dev:8080/api/jsonws/skinny-web.skinny/get-skinny-journal-articles/company-id/10155/group-name/Guest/ddm-structure-id/12602/locale/sv_SE',
+			url: 'http://localhost:5656/advice.json',
 			isJson: true
 		},
 		hbsDrugs: {
-			url: 'http://local.dev:8080/reklistan-theme/handlebars/details-drugs.hbs'
+			//url: 'http://local.dev:8080/reklistan-theme/handlebars/details-drugs.hbs'
+			url: 'http://localhost:5656/details-drugs.hbs',
 		}
 };
 
