0dbb6421ed5975ed490f8c47853c4e08e486cd71
Emil Öberg <emil.oberg@monator.com>
2015-08-17 17:09:44 +0200

    working example sidebar menu

--- src/app/views/dummy.js
+++ src/app/views/dummy.js
@@ -1,65 +1,81 @@
-const animationModule = require("ui/animation");
+import {Animation} from 'ui/animation';
 import {screen} from 'platform';
-import {inspect, saveFile} from './../shared/utils/debug';
-
 import {AbsoluteLayout} from 'ui/layouts/absolute-layout';
 
-let page;
 let elMenu;
+let elMainContent;
+let menuAnimation;
 
 const deviceWidth = screen.mainScreen.widthPixels / screen.mainScreen.scale;
 const deviceHeight = screen.mainScreen.heightPixels / screen.mainScreen.scale;
 
 function pageLoaded(args) {
-	page = args.object;
+	const page = args.object;
+
 	elMenu = page.getViewById('menuwrapper');
+	elMainContent = page.getViewById('maincontent');
 
 	AbsoluteLayout.setLeft(elMenu, deviceWidth); // Push menu off screen
 	elMenu.width = deviceWidth ; // Set menu to 100% width
-	elMenu.height = deviceHeight;
+	elMenu.height = deviceHeight; // Set menu to 100% height
 }
 
-function animateOut(args) {
-	console.log('Start Animate out!');
 
-	elMenu.animate({
-		//opacity: 0.75,
-		//rotate: 180,
-		//scale: { x: 2, y: 2 },
-		//backgroundColor: new colorModule.Color("Red"),
-		translate: { x: -deviceWidth, y: 0 },
-		duration: 250,
-		delay: 0,
-		iterations: 1,
-		curve: elMenu.ios ? UIViewAnimationCurve.UIViewAnimationCurveEaseIn : new android.view.animation.AccelerateInterpolator(1)
-	});
+function showMenu() {
+	const curve = elMenu.ios ? UIViewAnimationCurve.UIViewAnimationCurveEaseIn : new android.view.animation.AccelerateInterpolator(1);
+	const animationsSetup = [
+		{
+			target: elMenu,
+			translate: { x: -deviceWidth, y: 0 },
+			duration: 250,
+			delay: 0,
+			iterations: 1,
+			curve: curve
+		},
+		{
+			target: elMainContent,
+			opacity: 0.5,
+			duration: 250,
+			delay: 0,
+			iterations: 1,
+			curve: curve
+		}
+	];
+	const menuAnimation = new Animation(animationsSetup, false);
 
-	//var buttonAnimations = [
-	//	{ target: elMenu, translate: { x: -240, y: 0 }, scale: { x: 0.5, y: 0.5 }, opacity: 0, duration: 3000, delay: 0, iterations: 0, curve: curve }
-	//];
-	//let buttonAnimation = new animationModule.Animation(buttonAnimations, false);
-	//
-	////panelAnimation = panel.createAnimation({ opacity: 0, scale: { x: 0.5, y: 0.5 }, rotate: -360, backgroundColor: new colorModule.Color("red"), duration: vm.duration, iterations: vm.iterations });
-	//buttonAnimation.play().finished
-	//	//.then(function () { return panelAnimation.play().finished; })
-	//	.catch(function (e) { return console.log(e.message); });
-
+	menuAnimation.play().finished
+		.then(function () { return console.log('Animation done'); })
+		.catch(function (e) { return console.log(e.message); });
 }
 
-function whiteoutTap() {
-	elMenu.animate({
-		//opacity: 0.75,
-		//rotate: 180,
-		//scale: { x: 2, y: 2 },
-		//backgroundColor: new colorModule.Color("Red"),
-		translate: { x: 0, y: 0 },
-		duration: 250,
-		delay: 0,
-		iterations: 1,
-		curve: elMenu.ios ? UIViewAnimationCurve.UIViewAnimationCurveEaseIn : new android.view.animation.AccelerateInterpolator(1)
-	});
+
+function hideMenu() {
+	const curve = elMenu.ios ? UIViewAnimationCurve.UIViewAnimationCurveEaseIn : new android.view.animation.AccelerateInterpolator(1);
+	const animationsSetup = [
+		{
+			target: elMenu,
+			translate: { x: 0, y: 0 },
+			duration: 250,
+			delay: 0,
+			iterations: 1,
+			curve: curve
+		},
+		{
+			target: elMainContent,
+			opacity: 1,
+			duration: 250,
+			delay: 0,
+			iterations: 1,
+			curve: curve
+		}
+	];
+	const menuAnimation = new Animation(animationsSetup, false);
+
+	menuAnimation.play().finished
+		.then(function () { return console.log('Animation done'); })
+		.catch(function (e) { return console.log(e.message); });
 }
 
 exports.loaded = pageLoaded;
-exports.animateOut = animateOut;
-exports.whiteoutTap = whiteoutTap;
+exports.showMenuTap = showMenu;
+exports.hideMenuTap = hideMenu;
