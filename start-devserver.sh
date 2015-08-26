dac9d41384db38c3ea90679d292c837dff5ed630
Emil Öberg <emil.oberg@monator.com>
2015-08-26 17:02:22 +0200

    Removed all dev-resources-files as they're no longer usable. Now needs REK-listan Liferay server to connect to.

--- start-devserver.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-http-server -h >/dev/null 2>&1 || { echo >&2 "Please install http-server. Install with 'npm install -g http-server'."; exit 1; }
-http-server ./app/dev-resources -p5656
